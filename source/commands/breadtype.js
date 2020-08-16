469c33ce834baf422d908eb2f5416dee216dd5df
Coweh <67385236+Coweh@users.noreply.github.com>
2020-08-16 12:01:32 -0700

    Update breadtype.js
    I didn't even know 95% of these bread types existed

--- source/commands/breadtype.js
+++ source/commands/breadtype.js
@@ -1,10 +1,10 @@
 //Note: this command is far from finished, which is why it doesn't have a lot of bread types, and why it doesn't have anything in b!help.
 const baketype = ['baked', 'toasted', 'uncooked', 'cooked']
-const breadtype = ['aish merahrah', 'ajdov kruh', 'anpan', 'appam', 'arboud', 'arepa']
+const breadtype = ['aish merahrah', 'ajdov kruh', 'anpan', 'appam', 'arboud', 'arepa', 'baba', 'bagel', 'baguette', 'bakarkhani', 'balep korkun', 'bammy', 'banana', 'bannock', 'bara brith', 'barbari']
 module.exports = {
     name: 'breadtype',
     description: 'Gives you a random type of bread that may or may not exist.',
     execute(message, args) {
-        message.channel.send(baketype[Math.floor(Math.random() * 4)] + ' ' + breadtype[Math.floor(Math.random() * 6)] + ' bread')
+        message.channel.send(baketype[Math.floor(Math.random() * 4)] + ' ' + breadtype[Math.floor(Math.random() * 16)] + ' bread')
     },
 };
