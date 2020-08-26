23af7c8df6dacdb0add8114c894279cb10be3eec
Coweh <67385236+Coweh@users.noreply.github.com>
2020-08-25 21:08:44 -0700

    Update stickbugtrap.js
    a u t o d e l e t i o n

--- source/commands/stickbugtrap.js
+++ source/commands/stickbugtrap.js
@@ -3,15 +3,17 @@ module.exports = {
     description: 'Set a trap that will stick bug the first person to send a message in the channel this was used.',
     guildOnly: 'true',
     execute(message, args) {
+        const trapChannel = message.channel
         const stickBugger = message.author
-        message.author.send('The trap\'s been set. Now we just wait...')
+        message.delete()
+        stickBugger.send('The trap\'s been set. Now we just wait...')
         // Create a message collector that will end by DMing whoever ended said message collector.
         const filter = m => m.content.includes('');
-        const collector = message.channel.createMessageCollector(filter, { max: 1 });
+        const collector = message.channel.createMessageCollector(filter, {max: 1});
 
         collector.on('end', collected => {
-        message.channel.send(`You just got stick bugged by ${stickBugger} lol`);
-        message.channel.send({files:['https://cdn.discordapp.com/attachments/676934213156601899/745078718333976616/image0.gif']})
+        trapChannel.send(`You just got stick bugged by ${stickBugger} lol`);
+        trapChannel.send({files:['https://cdn.discordapp.com/attachments/676934213156601899/745078718333976616/image0.gif']})
         });
     },
 };
