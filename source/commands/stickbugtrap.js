b593a3f9b43d9c645b9a3feabc501bee92451463
Coweh <67385236+Coweh@users.noreply.github.com>
2020-08-25 20:57:08 -0700

    Update stickbugtrap.js
    Yeah I forgot to update this

--- source/commands/stickbugtrap.js
+++ source/commands/stickbugtrap.js
@@ -1,13 +1,13 @@
 module.exports = {
     name: 'stickbugtrap',
-    description: 'Set a 2-hour long trap that will stick bug the first person to send a message in the channel this was used.',
+    description: 'Set a trap that will stick bug the first person to send a message in the channel this was used.',
     guildOnly: 'true',
     execute(message, args) {
         const stickBugger = message.author
         message.author.send('The trap\'s been set. Now we just wait...')
         // Create a message collector that will end by DMing whoever ended said message collector.
         const filter = m => m.content.includes('');
-        const collector = message.channel.createMessageCollector(filter, { time: 7200000, max: 1 });
+        const collector = message.channel.createMessageCollector(filter, { max: 1 });
 
         collector.on('end', collected => {
         message.channel.send(`You just got stick bugged by ${stickBugger} lol`);
