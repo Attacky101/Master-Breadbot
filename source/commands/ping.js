7330f4040468a9d22144de5a77c703af5b62eaea
Coweh <67385236+Coweh@users.noreply.github.com>
2020-09-11 07:34:24 -0700

    Added category

--- source/commands/ping.js
+++ source/commands/ping.js
@@ -1,6 +1,7 @@
 module.exports = {
     name: 'ping',
     description: "Gets the bot's server ping.",
+    category: 'utility',
     execute(message, args) {
         var d = new Date()
         message.channel.send("**p o n g**").then((m) => {
