e27fab644c2aa368b457c520845ad168b33e365a
Coweh <67385236+Coweh@users.noreply.github.com>
2020-09-11 07:29:10 -0700

    Added category

--- source/commands/eggmansannouncement.js
+++ source/commands/eggmansannouncement.js
@@ -1,6 +1,7 @@
 module.exports = {
     name: 'eggmansannouncement',
     description: "I've come to make an announcement",
+    category: 'fun',
     execute(message, args) {
         message.channel.send("I've come to make an announcement: Shadow the Hedgehog can eat some flan");
     },
