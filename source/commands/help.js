6d689a06c2d9161435c152b77fd99605f52fca35
Coweh <67385236+Coweh@users.noreply.github.com>
2020-09-11 07:30:10 -0700

    Made b!help dynamically update and have categories

--- source/commands/help.js
+++ source/commands/help.js
@@ -1,13 +1,56 @@
 const Discord = require('discord.js');
+const { prefix } = require('../config.json')
 module.exports = {
     name: 'help',
     aliases: ['commands'],
-    description: 'Lists every command and their purposes.',
+    description: 'The command you\'re in right now.',
+    // left as false as args are optional.
+    args: false,
+    usage: '[command]',
+    category: 'utility',
     execute(message, args) {
+        const { commands } = message.client;
+        const funCommands = commands.filter(command => command.category === 'fun')
+        const utilCommands = commands.filter(command => command.category === 'utility')
+        const modCommands = commands.filter(command => command.category === 'moderation')
+        const data = []
         let embed = new Discord.MessageEmbed()
-        embed.setDescription("\`ping\` - Gets the bot's server ping.\n\`eggmansannouncement\` - I've come to make an announcement\n\`randomnumber`\ - Gives you a random number between a given range (whole numbers only).\n\`ooheeh`\ - I TOLD THE WITCH DOCTOR I WAS IN LOVE WITH YOU\n\`booah`\ - Boo ah\n\`bread`\ - Gives you a random picture of bread.\n\`changelog`\ - Shows the last three entries in the changelog.\n\`buge`\ - Gives you a random buge.\n\`trap` - Sets a trap that will send one of an assortment of messages to anyone who sends a message in the channel this command was used in.\n\`purge`\ - Deletes a given amount of messages.\n\`say`\ - Only usable by Coweh™#2517 or Cuno#3435. Makes Breadbot say something. Parts of the code taken from CunoBot (https://github.com/MikhailMCraft/CunoBot).\n\`kill`\ - Only usable by Coweh™#2517. Terminates the bot.")
-        embed.setTitle("Commands")
-        embed.setColor("FF0000")
-        message.channel.send(embed)
+        embed.setColor('FF0000')
+        if (!args.length) {
+            embed.setTitle('Categories')
+            embed.setDescription('\`Fun\`\n\`Utility\`\n\`Moderation\`')
+            embed.setFooter(`Type "${prefix}help <category>" for a list of commands in that category.`)
+            message.channel.send(embed)
+        }
+        else if (args[0] === 'Fun' || args[0] === 'fun') {
+            funCommands.forEach(command => data.push('`' + command.name + '` - ' + command.description))
+            embed.setTitle('Fun commands')
+            embed.setDescription(data.join('\n'))
+            message.channel.send(embed)
+        }
+        else if (args[0] === 'Utility' || args[0] == 'utility') {
+            utilCommands.forEach(command => data.push('`' + command.name + '` - ' + command.description))
+            embed.setTitle('Utility commands')
+            embed.setDescription(data.join('\n'))
+            message.channel.send(embed)
+        }
+        else if (args[0] === 'Moderation' || args[0] == 'moderation') {
+            modCommands.forEach(command => data.push('`' + command.name + '` - ' + command.description))
+            embed.setTitle('Moderation commands')
+            embed.setDescription(data.join('\n'))
+            message.channel.send(embed)
+        }
+        else {
+            message.channel.send('Uh')
+            setTimeout(function() {
+                message.channel.send('I hate to break it to you')
+            }, 2000)
+            setTimeout(function() {
+                message.channel.send('But uh')
+            }, 4000)
+            setTimeout(function() {
+                message.channel.send('that isn\'t really a help category')
+            }, 7000)
+        }
     },
 };
