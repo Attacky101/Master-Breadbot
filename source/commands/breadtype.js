cbcf22045cca89952f6b20897a4b3368d743fb53
Coweh <67385236+Coweh@users.noreply.github.com>
2020-09-11 15:29:40 -0700

    Revert "Revert "Added a few things""
    This reverts commit be98b58f5b7067e927b08b301c13b95397b31afb.

--- source/commands/breadtype.js
+++ source/commands/breadtype.js
@@ -1,4 +1,6 @@
-//Note: this command is far from finished, which is why it doesn't have a lot of bread types, and why it doesn't have anything in b!help.
+//Note: this command is far from finished, which is why it doesn't have a lot of bread types.
+const { randFromArray } = require("../functions");
+
 const baketype = ['Baked', 'Toasted', 'Uncooked', 'Cooked']
 const breadtype = ['aish merahrah', 'ajdov kruh', 'anpan', 'appam', 'arboud', 'arepa', 'baba', 'bagel', 'baguette', 'bakarkhani', 'balep korkun', 'bammy', 'banana', 'bannock', 'bara brith', 'barbari']
 module.exports = {
@@ -6,6 +8,6 @@ module.exports = {
     description: 'Gives you a random type of bread that may or may not exist.',
     category: 'fun',
     execute(message, args) {
-        message.channel.send(baketype[Math.floor(Math.random() * baketype.length)] + ' ' + breadtype[Math.floor(Math.random() * breadtype.length)] + ' bread')
+        message.channel.send(randFromArray(baketype) + ' ' + randFromArray(breadtype) + ' bread')
     },
 };
