ccd416e583d687a51c1905dd5e28e8512d3f470d
Coweh <67385236+Coweh@users.noreply.github.com>
2020-09-10 18:30:55 -0700

    Added new command
    yeah the code on this one may not be the greatest but i'm uh not amazing at js

---
+++ source/commands/lootsacc.js
@@ -0,0 +1,59 @@
+const normalEmotes = ['<:BubblyPensive:745438001152262286>']
+const normalText = ['**Pensive Bubbly** (Normal)']
+
+const superEmotes = ['<:unamusedsun:753775759822291004>']
+const superText = ['**Unamused Sun** (Super)']
+
+const highEndEmotes = ['<:pingkirby:684920141007683601>', '<:donkeypog:751497245618208991>']
+const highEndText = ['**Pinged Kirby** (High-End)', '**Donkey Pog** (High-End)']
+
+module.exports = {
+    name: 'lootsacc',
+    description: 'Have you ever wanted to open an Attacky Sack? Now you can.',
+    async execute(message, args) {
+        let emote1;
+        let emote2;
+        let text1;
+        let text2;
+        let textAndEmotesNum;
+
+        let rarityNum = Math.floor(Math.random() * 10);
+        if (rarityNum === 9) {
+            textAndEmotesNum = Math.floor(Math.random() * highEndEmotes.length);
+            emote1 = highEndEmotes[textAndEmotesNum];
+            text1 = highEndText[textAndEmotesNum]
+        }
+        else if (rarityNum === 6 || rarityNum === 7 || rarityNum === 8) {
+            textAndEmotesNum = Math.floor(Math.random() * superEmotes.length);
+            emote1 = superEmotes[textAndEmotesNum]
+            text1 = superText[textAndEmotesNum]
+        }
+        else {
+            textAndEmotesNum = Math.floor(Math.random() * normalEmotes.length);
+            emote1 = normalEmotes[textAndEmotesNum]
+            text1 = normalText[textAndEmotesNum]
+        }
+
+        rarityNum = Math.floor(Math.random() * 10);
+        if (rarityNum === 9) {
+            textAndEmotesNum = Math.floor(Math.random() * highEndEmotes.length);
+            emote2 = highEndEmotes[textAndEmotesNum];
+            text2 = highEndText[textAndEmotesNum]
+        }
+        else if (rarityNum === 6 || rarityNum === 7 || rarityNum === 8) {
+            textAndEmotesNum = Math.floor(Math.random() * superEmotes.length);
+            emote2 = superEmotes[textAndEmotesNum]
+            text2 = superText[textAndEmotesNum]
+        }
+        else {
+            textAndEmotesNum = Math.floor(Math.random() * normalEmotes.length);
+            emote2 = normalEmotes[textAndEmotesNum]
+            text2 = normalText[textAndEmotesNum]
+        }
+
+        const lootsaccMessage = await message.channel.send('**Opening Lootsacc...**\n<:sacc1:753687125391835296><:sacc2:753687125123399681>\n<:sacc3:753687125500887100><:sacc4:753687125106622465>')
+        setTimeout(function(){
+            lootsaccMessage.edit(`**Lootsacc opened!**\n<:sacc1:753687125391835296>${emote1}<:sacc2:753687125123399681>\n<:sacc3:753687125500887100>${emote2}<:sacc4:753687125106622465>\nYou got:\n-${text1}\n-${text2}`)
+        }, 2000)
+    },
+};
