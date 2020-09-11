be98b58f5b7067e927b08b301c13b95397b31afb
Coweh <67385236+Coweh@users.noreply.github.com>
2020-09-11 15:29:01 -0700

    Revert "Added a few things"
    This reverts commit 982e2ee7c5ed3f19cbfd5b90bdac9fd09e243fe7.

--- source/commands/breadtype.js
+++ source/commands/breadtype.js
@@ -1,6 +1,4 @@
-//Note: this command is far from finished, which is why it doesn't have a lot of bread types.
-const { randFromArray } = require("../functions");
-
+//Note: this command is far from finished, which is why it doesn't have a lot of bread types, and why it doesn't have anything in b!help.
 const baketype = ['Baked', 'Toasted', 'Uncooked', 'Cooked']
 const breadtype = ['aish merahrah', 'ajdov kruh', 'anpan', 'appam', 'arboud', 'arepa', 'baba', 'bagel', 'baguette', 'bakarkhani', 'balep korkun', 'bammy', 'banana', 'bannock', 'bara brith', 'barbari']
 module.exports = {
@@ -8,6 +6,6 @@ module.exports = {
     description: 'Gives you a random type of bread that may or may not exist.',
     category: 'fun',
     execute(message, args) {
-        message.channel.send(randFromArray(baketype) + ' ' + randFromArray(breadtype) + ' bread')
+        message.channel.send(baketype[Math.floor(Math.random() * baketype.length)] + ' ' + breadtype[Math.floor(Math.random() * breadtype.length)] + ' bread')
     },
 };
--- source/commands/buge.js
+++ source/commands/buge.js
@@ -1,5 +1,3 @@
-const { randFromArray } = require("../functions");
-
 const buge = [{files:["https://cdn.discordapp.com/attachments/715626295555260478/720453007099035669/buge.png"]}, {files:["https://cdn.discordapp.com/attachments/715626295555260478/720454694857867375/Screen_Shot_2020-06-10_at_6.46.04_PM.png"]}, {files:["https://cdn.discordapp.com/attachments/715626295555260478/720670129096228964/Screen_Shot_2020-06-11_at_9.05.13_AM.png"]}, {files:["https://cdn.discordapp.com/attachments/715626295555260478/720451870631395359/Screen_Shot_2020-06-10_at_6.37.39_PM.png"]}, {files:['https://cdn.discordapp.com/attachments/715626295555260478/745071250166251540/buge.png']}, {files:['https://media.discordapp.net/attachments/715626295555260478/745071250942197900/buge2.jpg']}, {files:['https://media.discordapp.net/attachments/715626295555260478/745071252967784468/buge3.jpg']}, {files:['https://media.discordapp.net/attachments/715626295555260478/745071256608702574/buge4.jpg']}, {files:['https://media.discordapp.net/attachments/715626295555260478/745071262287659018/buge5.jpg']}, {files:['https://media.discordapp.net/attachments/702372943396732989/744274362215170210/image1.jpg']}]
 module.exports = {
     name: 'buge',
@@ -12,7 +10,7 @@ module.exports = {
         }
         else {
             message.react('743245441667301477')
-            message.channel.send(`lookf at this stupteid BUGE\n`, randFromArray(buge))
+            message.channel.send(`lookf at this stupteid BUGE\n`, buge[Math.floor(Math.random() * buge.length)])
         }
     },
 };
