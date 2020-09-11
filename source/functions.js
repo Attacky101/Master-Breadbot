38e058361f02c402dfa1d0d2991b3daefb0a3cf7
Coweh <67385236+Coweh@users.noreply.github.com>
2020-09-11 15:28:59 -0700

    Revert "Added functions.js file"
    This reverts commit 3a73bf6b26c4b8cf823e864238777a481f8d3fe7.

--- source/functions.js
+++
@@ -1,8 +0,0 @@
-// This file defines various functions for easy use in other files.
-
-// This function will get a random element of an array.
-function randFromArray(array) {
-    return array[Math.floor(Math.random() * array.length)];
-}
-
-module.exports = { randFromArray }
