48c2c787141b19c0196b64cf5880fa3402fbc4e0
Coweh <67385236+Coweh@users.noreply.github.com>
2020-09-11 07:33:29 -0700

    Added category

--- source/commands/ooheeh.js
+++ source/commands/ooheeh.js
@@ -2,6 +2,7 @@ module.exports = {
     name: 'ooheeh',
     aliases: ['oohee', 'oohe'],
     description: 'I TOLD THE WITCH DOCTOR I WAS IN LOVE WITH YOU',
+    category: 'fun',
     execute(message, args) {
         message.channel.send('ooh eeh ooh ah ah ting tang walla walla bing bang')
     },
