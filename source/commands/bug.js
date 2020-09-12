bb0f66a2fdfba1d74760fd1b442ca9448227f200
Coweh <67385236+Coweh@users.noreply.github.com>
2020-09-12 07:48:54 -0700

    Added another command from a while ago

---
+++ source/commands/bug.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'bug',
+    description: 'they really put bug and not buge huh',
+    execute(message, args) {
+        message.channel.send('Bug?\n*Bug*?\nI think you mean *buge*, young fellow. Now don\'t make that mistake again. Understood?')
+    },
+};
